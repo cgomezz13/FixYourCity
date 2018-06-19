90dc1486ace7672aeb70b4c517fdf62fdf4e444e
vanillafrosty <dwu.inq@gmail.com>
2018-06-19 15:02:52 -0400

    add redux boilerplate

--- frontend/fix_your_city.jsx
+++ frontend/fix_your_city.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
-import LoginForm from './pages/LoginForm'
+// import LoginForm from './pages/LoginForm';
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<LoginForm />, document.getElementById('root'))
-})
+  let store;
+  if (window.currentUser) {
+    let preloadedState = {
+      entities: {
+        users: {[window.currentUser.id]: window.currentUser}
+      },
+      session: {id: window.currentUser.id},
+    };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+  ReactDOM.render(<Root />, document.getElementById('root'))
+});
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+
+const obj = {};
+
+export const rootReducer = combineReducers(obj);
