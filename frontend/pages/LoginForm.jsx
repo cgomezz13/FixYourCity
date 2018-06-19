b19848a9712a2511c683c95c1d8b64bc8a5ce944
yeti-detective <yeti.detective@gmail.com>
2018-06-19 13:23:20 -0400

    deleted index.html because this is a rails app, adding LoginForm.jsx

---
+++ frontend/pages/LoginForm.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.submitLogin = this.submitLogin.bind(this)
+  }
+
+  submitLogin (e) {
+    e.preventDefault()
+    console.log('submitting login (not really)')
+  }
+
+  updateName (e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  updatePW (e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <form className="login" onSubmit={this.submitLogin}>
+
+      </form>
+    )
+  }
+}
