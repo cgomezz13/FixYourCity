69e245cbdc446103e1b86637d93fababf52dcf15
yeti-detective <yeti.detective@gmail.com>
2018-06-19 17:22:49 -0400

    added photo S3 photo attachments to incident model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,6 +2,9 @@ class Incident < ApplicationRecord
   validates :name, :location, presence: true, uniqueness: true
   validates :description, presence: true
 
+  has_attached_file :photo
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
+
   has_many :comments,
     class_name: 'Comment',
     foreign_key: :incident_id,
