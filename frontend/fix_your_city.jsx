a796e1ba163457f2bd35d443d57d5faff7df49ca
yeti-detective <yeti.detective@gmail.com>
2018-06-19 19:56:46 -0400

    restore root js file to pre-test configuration

--- frontend/fix_your_city.jsx
+++ frontend/fix_your_city.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './pages/root';
-import MapPage from './components/map_page';
-// import configureStore from './store/store';
+// import MapPage from './components/map_page';
+import configureStore from './store/store';
 
 
-// import { login, logout, signUp } from "./util/session_api_util";
-// import LoginForm from './pages/LoginForm';
+import { login, logout, signUp } from "./util/session_api_util";
+import LoginForm from './pages/LoginForm';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // let store;
-  // if (window.currentUser) {
-  //   let preloadedState = {
-  //     entities: {
-  //       users: { [window.currentUser.id]: window.currentUser }
-  //     },
-  //     session: { id: window.currentUser.id }
-  //   };
-  //   store = configureStore(preloadedState);
-  // } else {
-  //   store = configureStore();
-  // }
-  // ReactDOM.render(<Root />, document.getElementById('root'))
-  ReactDOM.render(<MapPage />, document.getElementById('root'))
+  let store;
+  if (window.currentUser) {
+    let preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+  ReactDOM.render(<Root />, document.getElementById('root'))
+  // ReactDOM.render(<MapPage />, document.getElementById('root'))
 
 });
