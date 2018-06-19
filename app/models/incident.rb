7af8aaac258e50a14f9a24ecbd56bc43e4e8b0f4
Cindy <c.puchex@gmail.com>
2018-06-19 13:38:32 -0400

    added comment association to incident

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,9 @@ class Incident < ApplicationRecord
   validates :name, :location, presence: true, uniqueness: true
   validates :description, presence: true
 
+  has_many :comments,
+    class_name: 'Comment',
+    foreign_key: :incident_id,
+    primary_key: :id
+
 end
