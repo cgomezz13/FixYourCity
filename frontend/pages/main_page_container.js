1febb6cf32356852fa7897e7b0414a3b02402831
Joey Wu <wjoeyu@gmail.com>
2018-06-19 19:53:08 -0400

    started skeleton of main_page

---
+++ frontend/pages/main_page_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import MainPage from "./main_page";
+
+const mSp = (state) => {
+    return {
+
+    };
+};
+
+const mDp = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(mSp, mDp)(MainPage);
