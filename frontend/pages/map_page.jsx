d3d01d61cdf4848ce5a2cc53b16eefefda35599e
yeti-detective <yeti.detective@gmail.com>
2018-06-19 18:47:42 -0400

    map page ready to test

--- frontend/pages/map_page.jsx
+++ frontend/pages/map_page.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
 
-// const tomtom = require('')
-
 export default class MapPage extends Component {
   constructor() {
     super()
   }
 
   componentDidMount() {
-    console.log(tomtom)
+    tomtom.setProductInfo('fix-your-city', '1.0');
+    const map = tomtom.map('map', {
+      key: 'ZzhLoHMvN7NYtV6UorNZ3BAWwGDSgbfh',
+      source: 'vector',
+      basePath: '../../jssdk-4.29.0-distribution'
+    });
+    const languageLabel = L.DomUtil.create('label');
+    languageLabel.innerHTML = 'Maps language';
+    const languageSelector = tomtom.languageSelector.getHtmlElement(tomtom.globalLocaleService, 'maps');
+    languageLabel.appendChild(languageSelector);
+    tomtom.controlPanel({
+      position: 'bottomright',
+      title: 'Settings',
+      collapsed: true,
+      closeOnMapClick: false
+    })
+      .addTo(map)
+      .addContent(languageLabel);
   }
 
   render () {
