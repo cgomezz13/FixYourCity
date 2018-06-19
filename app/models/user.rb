bda3a2ecea06ace1e5aae8ec43728b84bddb7f65
yeti-detective <yeti.detective@gmail.com>
2018-06-19 14:54:35 -0400

    added ability to attach images

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
+  has_attached_file :avatar, default_url: "default_avi.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   has_many :comments,
     class_name: 'Comment',
     foreign_key: :user_id,
