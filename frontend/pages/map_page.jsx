69d2638bb4db100c182d02404268e5fba06f0116
yeti-detective <yeti.detective@gmail.com>
2018-06-19 17:50:44 -0400

    building a map page

---
+++ frontend/pages/map_page.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class MapPage extends Component {
+  constructor() {
+    super()
+  }
+
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
