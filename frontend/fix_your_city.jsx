bb912fabeb365889600f0fd91aa854118c45d4d3
Cindy <c.puchex@gmail.com>
2018-06-19 16:57:13 -0400

    testing user ajax requests

--- frontend/fix_your_city.jsx
+++ frontend/fix_your_city.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './pages/root';
-import configureStore from './store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./pages/root";
+import configureStore from "./store/store";
 
+// import { login, logout, signUp } from "./util/session_api_util";
 // import LoginForm from './pages/LoginForm';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
     let preloadedState = {
       entities: {
-        users: {[window.currentUser.id]: window.currentUser}
+        users: { [window.currentUser.id]: window.currentUser }
       },
-      session: {id: window.currentUser.id},
+      session: { id: window.currentUser.id }
     };
     store = configureStore(preloadedState);
   } else {
     store = configureStore();
   }
-  ReactDOM.render(<Root />, document.getElementById('root'))
+
+  ReactDOM.render(<Root />, document.getElementById("root"));
 });
