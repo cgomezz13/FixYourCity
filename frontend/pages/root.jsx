485869f75d638eb0812db12c94384fffdbb064ce
yeti-detective <yeti.detective@gmail.com>
2018-06-19 23:52:32 -0400

    testing new incident form

--- frontend/pages/root.jsx
+++ frontend/pages/root.jsx
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 import App from './app';
+import NewIncidentForm from '../components/new_incident_form_container';
 
 const Root = (props) => {
   return (
     <Provider store={props.store}>
       <HashRouter>
         <App />
+        <NewIncidentForm />
       </HashRouter>
     </Provider>
   );
