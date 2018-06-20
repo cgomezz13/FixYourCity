21931cf3d99c75ea042921ad0670d9a6ad57e083
yeti-detective <yeti.detective@gmail.com>
2018-06-20 09:14:44 -0400

    put login button as child of appropriate component

--- frontend/pages/home_container.jsx
+++ frontend/pages/home_container.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import NewIncidentForm from '../components/new_incident_form_container';
+import LoginButton from '../components/login_button_container';
+
 const HomeContainer = (props) => {
   return (
     <div className="home-container">
-      Hello World!
+      <NewIncidentForm />
+      <LoginButton />
     </div>
   );
 };
