cf5a7b064c0e2d2dc396f36f26a98eaf1df5db31
Cindy <c.puchex@gmail.com>
2018-06-20 12:13:48 -0400

    redirecting after successful login

--- frontend/components/login_button.jsx
+++ frontend/components/login_button.jsx
@@ -1,42 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class LoginButton extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       errors: null
     };
     this.submitLogin = this.submitLogin.bind(this);
   }
 
-  submitLogin (e) {
+  submitLogin(e) {
     e.preventDefault();
-    this.props.login({
-      username: 'demo_user',
-      password: 'starwars'
-    })
-      .then(null, (err) => {
+    debugger;
+    this.props
+      .login({
+        username: "demo_user",
+        password: "starwars"
+      })
+      .then(this.props.history.push("/"), err => {
         this.setState({
           errors: err.resposeText
-        })
-      })
+        });
+      });
   }
 
-  render () {
+  render() {
     return (
-      <form
-        className="login-button"
-        onSubmit={this.submitLogin}
-        >
-          { this.state.errors && (
-            <h2>{this.state.errors}</h2>
-          )}
-        <button
-          value="Demo User"
-          >
-          Demo User
-        </button>
+      <form className="login-button" onSubmit={this.submitLogin}>
+        {this.state.errors && <h2>{this.state.errors}</h2>}
+        <button value="Demo User">Demo User</button>
       </form>
-    )
+    );
   }
 }
