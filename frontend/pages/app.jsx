0096bcdfdb3464040bfaccc700c1a69ad4d88183
Cindy <c.puchex@gmail.com>
2018-06-20 09:09:22 -0400

    styling changed

--- frontend/pages/app.jsx
+++ frontend/pages/app.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 // import SignupForm from './session/signup_form_container';
-import LoginForm from './session/LoginForm';
+import LoginForm from "./session/LoginForm";
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import HomeContainer from './home_container';
-import NewIncidentForm from '../components/new_incident_form_container';
+import HomeContainer from "./home_container";
+import NewIncidentForm from "../components/new_incident_form_container";
 
-
-
-const App = (props) => {
+const App = props => {
   return (
     <div className="appContainer">
       <Switch>
