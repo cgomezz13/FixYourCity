e3ba073b386c4e2b141b4bfe57d00d19c9bc8d9a
Cindy <c.puchex@gmail.com>
2018-06-20 10:48:19 -0400

    removed testing and change preloaded state

--- frontend/fix_your_city.jsx
+++ frontend/fix_your_city.jsx
@@ -4,22 +4,12 @@ import Root from "./pages/root";
 // import MapPage from './components/map_page';
 import configureStore from "./store/store";
 
-import { login, logout, signUp } from "./util/session_api_util";
-import {
-  createIncident,
-  updateIncident,
-  allIncidents
-} from "./util/incident_api_util";
-import { createComment, deleteComment } from "./util/comment_api_util";
 // import LoginForm from './pages/LoginForm';
 
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
     let preloadedState = {
-      entities: {
-        users: { [window.currentUser.id]: window.currentUser }
-      },
       session: { id: window.currentUser.id }
     };
     store = configureStore(preloadedState);
@@ -27,14 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore();
   }
 
-  // window.login = login;
-  // window.logout = logout;
-  // window.signUp = signUp;
-  // window.createIncident = createIncident;
-  // window.updateIncident = updateIncident;
-  // window.allIncidents = allIncidents;
-  // window.createComment = createComment;
-  // window.deleteComment = deleteComment;
   window.store = store;
 
   ReactDOM.render(<Root store={store} />, document.getElementById("root"));
