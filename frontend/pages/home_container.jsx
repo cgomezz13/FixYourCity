3e94e6527f65a985833ee15e863ee137465757c2
yeti-detective <yeti.detective@gmail.com>
2018-06-20 09:53:25 -0400

    map on screen, help

--- frontend/pages/home_container.jsx
+++ frontend/pages/home_container.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import NewIncidentForm from '../components/new_incident_form_container';
 import LoginButton from '../components/login_button_container';
+import MapPage from '../components/map_page';
 
 const HomeContainer = (props) => {
   return (
     <div className="home-container">
       <NewIncidentForm />
+      <MapPage />
       <LoginButton />
     </div>
   );
