869edcd287d520627aeafc76e79706032c215068
yeti-detective <yeti.detective@gmail.com>
2018-06-20 00:17:23 -0400

    that wasn't the right place to put the form for testing

--- frontend/pages/root.jsx
+++ frontend/pages/root.jsx
@@ -3,14 +3,12 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 import App from './app';
-import NewIncidentForm from '../components/new_incident_form_container';
 
 const Root = (props) => {
   return (
     <Provider store={props.store}>
       <HashRouter>
         <App />
-        <NewIncidentForm />
       </HashRouter>
     </Provider>
   );
