1dd647f7da71fbba56532b5829d18a1c62d741b8
yeti-detective <yeti.detective@gmail.com>
2018-06-20 00:17:44 -0400

    this is the right place to put the form for testing

--- frontend/pages/app.jsx
+++ frontend/pages/app.jsx
@@ -4,12 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import LoginForm from './session/LoginForm';
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import HomeContainer from './home_container';
+import NewIncidentForm from '../components/new_incident_form_container';
+
 
 
 const App = (props) => {
   return (
     <div className="appContainer">
       <Switch>
+        <NewIncidentForm />
         <Route path="/login" component={LoginForm} />
         <Route path="/" component={HomeContainer} />
       </Switch>
