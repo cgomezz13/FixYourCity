312bc367620ebbb1eb8e1900fdfe911f6d2cb3e0
yeti-detective <yeti.detective@gmail.com>
2018-06-22 21:20:26 -0400

    small layout changes

--- frontend/components/new_incident_form.jsx
+++ frontend/components/new_incident_form.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class NewIncidentForm extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       errors: null,
       newIncident: {
@@ -13,7 +13,7 @@ export default class NewIncidentForm extends Component {
         photo: null
       },
       img_url: null
-    }
+    };
     this.submitIncident = this.submitIncident.bind(this);
     this.updateDescription = this.updateDescription.bind(this);
     this.updateLocation = this.updateLocation.bind(this);
@@ -22,43 +22,45 @@ export default class NewIncidentForm extends Component {
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      const newIncident = Object.assign({}, this.state.newIncident)
+    navigator.geolocation.getCurrentPosition(pos => {
+      const newIncident = Object.assign({}, this.state.newIncident);
       const position = pos;
       newIncident.lat = position.coords.latitude;
       newIncident.lon = position.coords.longitude;
       this.setState({
         newIncident: newIncident
-      })
-    })
+      });
+    });
   }
 
   submitIncident(e) {
     e.preventDefault();
     let formData = new FormData();
     if (this.state.newIncident.photo) {
-      formData.append('incident[photo]', this.state.newIncident.photo)
+      formData.append('incident[photo]', this.state.newIncident.photo);
     }
     formData.append('incident[name]', this.state.newIncident.name);
     formData.append('incident[lat]', this.state.newIncident.lat);
     formData.append('incident[lon]', this.state.newIncident.lon);
-    formData.append('incident[description]', this.state.newIncident.description);
+    formData.append(
+      'incident[description]',
+      this.state.newIncident.description
+    );
     // // const newIncident = Object.assign({}, this.state.newIncident)
     // if (newIncident.photo === null) {
     //   delete newIncident.photo;
     // }
-    this.props.createIncident(formData)
-      .then(null, (err) => {
-        this.setState({
-          errors: err.responseText
-        })
-      })
+    this.props.createIncident(formData).then(null, err => {
+      this.setState({
+        errors: err.responseText
+      });
+    });
   }
 
   updateField(field, value) {
     const newState = Object.assign({}, this.state.newIncident);
-    newState[field] = value
-    this.setState({newIncident: newState})
+    newState[field] = value;
+    this.setState({ newIncident: newState });
   }
 
   updateDescription(e) {
@@ -76,15 +78,15 @@ export default class NewIncidentForm extends Component {
   updatePhoto(e) {
     const photo = e.currentTarget.files[0];
     const fileReader = new FileReader();
-    const newIncident = Object.assign({}, this.state.newIncident)
-    newIncident.photo = photo
+    const newIncident = Object.assign({}, this.state.newIncident);
+    newIncident.photo = photo;
 
     fileReader.onloadend = () => {
       this.setState({
         newIncident: newIncident,
         img_url: fileReader.result
-      })
-    }
+      });
+    };
     if (photo) {
       fileReader.readAsDataURL(photo);
     }
@@ -93,47 +95,49 @@ export default class NewIncidentForm extends Component {
   render() {
     return (
       <form className="new-incident-form" onSubmit={this.submitIncident}>
-        <h1>New Incident Form</h1>
-        { this.state.errors && (
-          <h2 style={{color: '#f00'}}>{this.state.errors}</h2>
+        <h3>New Incident Form</h3>
+        {this.state.errors && (
+          <h2 style={{ color: '#f00' }}>{this.state.errors}</h2>
         )}
-          <label>Post Title<br />
+        <label>
+          Post Title<br />
           <input
             type="text"
             placeholder="give a descriptive title"
             onChange={this.updateName}
             value={this.state.name}
-            />
-        </label><br />
-        <label>Description<br/>
+          />
+        </label>
+        <br />
+        <label>
+          Description<br />
           <textarea
             placeholder="give a concise description"
             onChange={this.updateDescription}
             value={this.state.description}
-            />
-        </label><br />
-        <label>Take Photo<br />
+          />
+        </label>
+        <br />
+        <label>
+          Take Photo<br />
           <input
             type="file"
             accept="image/*"
             capture="environment"
             onChange={this.updatePhoto}
-            />
-        </label><br />
-        <label>Upload Photo<br />
-          <input
-            type="file"
-            accept="image/*"
-            onChange={this.updatePhoto}
-            />
-        </label><br />
-        {
-          this.state.img_url && (
-            <img src={this.state.img_url} className="image-preview" />
-          )
-        }
+          />
+        </label>
+        <br />
+        <label>
+          Upload Photo<br />
+          <input type="file" accept="image/*" onChange={this.updatePhoto} />
+        </label>
+        <br />
+        {this.state.img_url && (
+          <img src={this.state.img_url} className="image-preview" />
+        )}
         <input type="submit" value="Submit Incident" />
       </form>
-    )
+    );
   }
 }
