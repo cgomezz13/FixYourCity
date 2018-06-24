5fc288c0e484d2a6c53216eb68b3e3f2fefedcc1
yeti-detective <yeti.detective@gmail.com>
2018-06-24 12:40:30 -0400

    changed attachement name to differentiate from paperclip

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -3,9 +3,9 @@ class Incident < ApplicationRecord
   validates :description, presence: true
 
   # has_attached_file :photo
-  has_one_attached :photo
+  has_one_attached :photo_as
   # validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
-  validates_acceptance_of :photo, content_type: /\Aimage\/.*\z/
+  validates_acceptance_of :photo_as, content_type: /\Aimage\/.*\z/
 
   has_many :comments,
     class_name: 'Comment',
